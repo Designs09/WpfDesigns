5900c2c47146af19568c47be4db8f016b05c8a91
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2020-04-09 22:34:15 +0800

    0

---
+++ Fasetto.Word/Fasetto.Word/ViewModel/BaseViewModel.cs
@@ -0,0 +1,22 @@
+﻿using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fasetto.Word
+{
+    /// <summary>
+    /// A base view model that fires Property Changed events as needed
+    /// </summary>
+    [AddINotifyPropertyChangedInterface]
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// The event that fired when any child property changes its value
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
+    }
+}
