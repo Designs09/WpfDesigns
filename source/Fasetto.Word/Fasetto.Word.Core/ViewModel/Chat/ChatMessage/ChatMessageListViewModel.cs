508614baecb211a67df5ef495bcbe33418d01f52
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2020-04-26 23:13:30 +0800

    feature
    ①消息列表绑定到视图模型。
    ②添加读取时间，发送时间

---
+++ source/Fasetto.Word/Fasetto.Word.Core/ViewModel/Chat/ChatMessage/ChatMessageListViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fasetto.Word.Core
+{
+    /// <summary>
+    /// A view model for chat message thread list
+    /// </summary>
+    public class ChatMessageListViewModel : BaseViewModel
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// The chat thread items for the list
+        /// </summary>
+        public List<ChatMessageListItemViewModel> Items { get; set; }
+
+        #endregion
+    }
+}
---
+++ source/Fasetto.Word/Fasetto.Word/ValueConverters/TimeToReadTimeConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+
+namespace Fasetto.Word
+{
+    /// <summary>
+    /// A converter that takes in a date and converts it to a user friendly message read time
+    /// </summary>
+    public class TimeToReadTimeConverter : BaseValueConverter<TimeToReadTimeConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var time = (DateTimeOffset)value;
+
+            // If it is not read...
+            if (time == DateTimeOffset.MinValue)
+                return string.Empty;
+
+            //If it is today...
+            if (time.Date == DateTimeOffset.UtcNow.Date)
+            {
+                // Return just time
+                var rd = time.ToLocalTime().ToString("HH:mm tt", CultureInfo.GetCultureInfo("en-us"));
+                return $"Read {rd}";
+            }
+
+            // Otherwise, return a full date
+            var readTime = time.ToLocalTime().ToString("HH:mm, MMM yyyy", CultureInfo.GetCultureInfo("en-us"));
+            return $"Read {readTime}";
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
