195367d025d3798c5e6031cf9071733d38c7aa3c
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2020-05-06 23:05:40 +0800

    new
    添加列表控件演示

---
+++ source/ListDemo/ListDemo.Core/ViewModel/Device/DeviceListItemViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ListDemo.Core
+{
+    public class DeviceListItemViewModel : BaseViewModel
+    {
+        public string Name { get; set; }
+        public string Path { get; set; }
+        public DeviceType Type { get; set; }
+
+        public ICommand ClickCommand { get; set; }
+
+        public DeviceListItemViewModel()
+        {
+            ClickCommand = new RelayCommand(Click);
+        }
+
+        private void Click()
+        {
+        }
+    }
+}
