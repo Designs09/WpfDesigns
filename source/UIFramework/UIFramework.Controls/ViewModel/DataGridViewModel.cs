a7ec7c0784843bf3e6481eac63849e53a1831399
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2020-05-08 17:20:47 +0800

    feature
    UIFramework添加DataGrid控件样例。
    框架更新，添加了ValueConverters模板。

---
+++ source/UIFramework/UIFramework.Controls/ViewModel/DataGridViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UIFramework.Core;
+
+namespace UIFramework.Controls
+{
+    public class DataGridViewModel : BaseViewModel
+    {
+        public static DataGridViewModel Instance = new DataGridViewModel();
+
+        public List<DataGridItemViewModel> Items { get; set; }
+
+        public DataGridViewModel()
+        {
+            Items = new List<DataGridItemViewModel> 
+            { 
+                new DataGridItemViewModel(),
+                new DataGridItemViewModel(),
+                new DataGridItemViewModel(),
+                new DataGridItemViewModel(),
+                new DataGridItemViewModel(),
+            };
+        }
+    }
+
+    public class DataGridItemViewModel : BaseViewModel
+    {
+        public int Value { get; set; }
+        public string Name { get; set; }
+
+        public DataGridItemViewModel()
+        {
+            Value = 5;
+            Name = "Miki";
+        }
+    }
+}
