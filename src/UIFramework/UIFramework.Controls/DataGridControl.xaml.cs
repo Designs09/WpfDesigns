c8f455b3a569fab82ccf6ad4302aa27047e22a22
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2020-06-24 19:47:36 +0800

    feature
    DataGrid行选中探索

--- src/UIFramework/UIFramework.Controls/DataGridControl.xaml.cs
+++ src/UIFramework/UIFramework.Controls/DataGridControl.xaml.cs
@@ -24,5 +24,10 @@ namespace UIFramework.Controls
         {
             InitializeComponent();
         }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            MessageBox.Show("Hello World!");
+        }
     }
 }
