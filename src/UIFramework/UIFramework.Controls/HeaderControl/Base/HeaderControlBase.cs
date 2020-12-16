dda0f83945d50393f3f9d3c1c320c27295484d66
Ganwei_RD-09 <Ganwei_RD-09@LAPTOP-TFRH16I6>
2020-12-16 16:27:34 +0800

    feature：标题控件

---
+++ src/UIFramework/UIFramework.Controls/HeaderControl/Base/HeaderControlBase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace UIFramework.Controls.HeaderControl
+{
+    public class HeaderControlBase : ContentControl
+    {
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Title.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register("Title", typeof(string), typeof(HeaderControlBase), new PropertyMetadata("水资源水水水水水水水水系统"));
+
+
+
+        public string SubTitle
+        {
+            get { return (string)GetValue(SubTitleProperty); }
+            set { SetValue(SubTitleProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for SubTitle.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SubTitleProperty =
+            DependencyProperty.Register("SubTitle", typeof(string), typeof(HeaderControlBase), new PropertyMetadata("Water water water water water water water system"));
+    }
+}
