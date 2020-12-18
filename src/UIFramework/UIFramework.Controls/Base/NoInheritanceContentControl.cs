98928f18c64fea682af20c9cca4d3ec1022677ef
Ganwei_RD-09 <Ganwei_RD-09@LAPTOP-TFRH16I6>
2020-12-18 10:56:46 +0800

    feature：不继承父控件样式

---
+++ src/UIFramework/UIFramework.Controls/Base/NoInheritanceContentControl.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace UIFramework.Controls
+{
+    /// <summary>
+    /// 不继承父控件的样式
+    /// </summary>
+    public class NoInheritanceContentControl : ContentControl
+    {
+        public NoInheritanceContentControl()
+        {
+            this.InheritanceBehavior = System.Windows.InheritanceBehavior.SkipAllNow;
+        }
+    }
+
+    // 例：
+    
+    //<local:NoInheritanceContentControl>
+    //    <local:ExhibitionPage />
+    //</local:NoInheritanceContentControl>
+}
